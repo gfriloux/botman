a18b60e00db075515b15a450696ee6e8f29237f2
Guillaume Friloux <guillaume@friloux.me>
2015-04-22 16:47:29 +0200

    Make libgothan an installable lib to package it separately.

--- src/lib/Makefile.mk
+++ src/lib/Makefile.mk
@@ -1,21 +1,36 @@
-MAINTAINERCLEANFILES += \
+MAINTAINERCLEANFILES +=                                                        \
 src/lib/gotham/*.gc{no,da}
 
-noinst_LTLIBRARIES = \
+if BUILD_GOTHAM
+include_HEADERS = src/include/Gotham.h
+
+noinst_LTLIBRARIES =                                                           \
+src/lib/libcjson.la
+
+lib_LTLIBRARIES =                                                              \
 src/lib/libgotham.la
 
-src_lib_libgotham_la_SOURCES = \
-src/lib/gotham/gotham.c \
-src/lib/gotham/gotham_private.h \
-src/lib/gotham/citizen.c \
-src/lib/gotham/command.c \
-src/lib/gotham/conf.c \
-src/lib/gotham/emoticons.c \
-src/lib/gotham/event.c \
-src/lib/gotham/modules.c \
-src/lib/gotham/modules_conf.c \
-src/lib/gotham/utils.c
-src_lib_libgotham_la_CFLAGS = \
-$(GOTHAM_CFLAGS) $(AM_CPPFLAGS) \
+src_lib_libgotham_la_SOURCES =                                                 \
+   src/lib/gotham/gotham.c                                                     \
+   src/lib/gotham/gotham_private.h                                             \
+   src/lib/gotham/citizen.c                                                    \
+   src/lib/gotham/command.c                                                    \
+   src/lib/gotham/conf.c                                                       \
+   src/lib/gotham/emoticons.c                                                  \
+   src/lib/gotham/event.c                                                      \
+   src/lib/gotham/modules.c                                                    \
+   src/lib/gotham/modules_conf.c                                               \
+   src/lib/gotham/utils.c                                                      \
+   src/include/Gotham.h
+src_lib_libgotham_la_CFLAGS =                                                  \
+$(GOTHAM_CFLAGS) $(AM_CPPFLAGS)                                                \
 -DGOTHAM_MODULE_PATH=\"$(libdir)/gotham/$(MODULE_ARCH)\"
-src_lib_libgotham_la_LDFLAGS = $(GOTHAM_LIBS)
+src_lib_libgotham_la_LDFLAGS = $(GOTHAM_LIBS)                                  \
+   -version-info @version_info@ -no-undefined
+src_lib_libgotham_la_LIBADD =                                                  \
+   src/lib/libcjson.la
+
+src_lib_libcjson_la_SOURCES =                                                  \
+   src/lib/extras/cJSON.c                                                      \
+   src/include/cJSON.h
+endif
