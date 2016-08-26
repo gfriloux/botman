59d054d2dd989e4dff8dcd81382750bf18d64ad3
Guillaume Friloux <guillaume@friloux.me>
2016-08-26 15:49:24 +0200

    gdb: Fix build.

--- src/modules/gdb/event.c
+++ src/modules/gdb/event.c
@@ -22,8 +22,10 @@
 Eina_Bool
 event_connect(void *data,
               int type EINA_UNUSED,
-              void *ev)
+              void *ev EINA_UNUSED)
 {
+   Module_Gdb *gdb = data;
+
    botman_dumps_poll(gdb);
    return EINA_TRUE;
 }
