e5fe3ea53f5bc80ba2fb29e6a9b4e04c54e4bbf8
Guillaume Friloux <guillaume@friloux.me>
2016-09-30 16:56:37 +0200

    log: Avoid double string escape when adding a log.

--- src/modules/log/esql.c
+++ src/modules/log/esql.c
@@ -19,16 +19,37 @@ log_esql_add(Log *log,
              const char *message)
 {
    Esql_Query_Id id;
+   Eina_Strbuf *query,
+               *bsource,
+               *bmessage;
 
    DBG("log[%p] type[%d] source[%s] message[%s]", log, type, source, message);
 
    EINA_SAFETY_ON_TRUE_RETURN_VAL(!log->bdd.e, EINA_TRUE);
    EINA_SAFETY_ON_TRUE_RETURN_VAL(!log->bdd.connected, EINA_TRUE);
 
-   id = esql_query_args(log->bdd.e, log,
-                        "INSERT INTO log ('type', 'source', 'data') "
-                                 "VALUES ('%d', '%s', '%s');",
-                        type, source, message);
+   query = eina_strbuf_new();
+   bsource = eina_strbuf_new();
+   bmessage = eina_strbuf_new();
+
+   eina_strbuf_append(bsource, source ? source : "");
+   eina_strbuf_replace_all(bsource, "'", "''");
+
+   eina_strbuf_append(bmessage, message ? message : "");
+   eina_strbuf_replace_all(bmessage, "'", "''");
+
+   eina_strbuf_append_printf(query,
+                             "INSERT INTO log ('type', 'source', 'data') "
+                                      "VALUES ('%d', '%s', '%s');",
+                             type, eina_strbuf_string_get(bsource),
+                             eina_strbuf_string_get(bmessage));
+   eina_strbuf_free(bsource);
+   eina_strbuf_free(bmessage);
+
+   DBG("query[%s]", eina_strbuf_string_get(query));
+
+   id = esql_query(log->bdd.e, log, eina_strbuf_string_get(query));
+   eina_strbuf_free(query);
    EINA_SAFETY_ON_TRUE_RETURN_VAL(!id, EINA_TRUE);
 
    esql_query_callback_set(id, _log_esql_add_cb);
